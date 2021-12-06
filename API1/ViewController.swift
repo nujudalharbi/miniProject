dfc089b942cb8dd1bec6e9d6e4f254ff3aeb4a50
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-12-06 13:38:22 +0300

    build class and two struct

--- API1/ViewController.swift
+++ API1/ViewController.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+class APIResponse : Codable{
+  let total : Int
+  let total_pages : Int
+  let results : [Result]
+}
+  struct Result : Codable {
+   let id : String
+    let urls : URLS
+  }
+struct URLS : Codable{
+  let full : String
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
